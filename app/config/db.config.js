0858077be04bd0bb42c5504eb9caa1dadcc1af14
ashikurcmt126 <ashikurcmt126@gmail.com>
2021-05-25 13:32:19 +0600

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "ashikur",
+  DB: "nodejs-test",
+  PGPORT: 5433,
+  dialect: "postgres",
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
